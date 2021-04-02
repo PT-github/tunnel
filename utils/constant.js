023eaf62b8384d43812366f6bb28b6ea80a5dd2d
wangsitu <996803783@qq.com>
2021-04-02 15:46:44 +0800

    socket链接修改

--- utils/constant.js
+++ utils/constant.js
@@ -5,4 +5,4 @@ export const BASE_SERVER = '/api'
 // export const BASE_SERVER = 'http://172.16.22.196:18080'
 
 // export const SOCKET_SERVER = 'w8.qcxt.com:65036/api'
-export const SOCKET_SERVER = top.window.location.host
+export const SOCKET_SERVER = `${top.window.location.host}/api`
